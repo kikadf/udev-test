d117c48b7aa3532208ebd1c9ab1701909a938d39
kikadf <kikadf.01@gmail.com>
2024-10-28 11:22:13 +0100

    Add some tests

--- Makefile
+++ Makefile
@@ -1,18 +1,21 @@
-CC = gcc
-CFLAGS = -g -Wall
+CC = 		gcc
+CFLAGS = 	-g -Wall -I/usr/include -I/usr/local/include
+LDFLAGS =	-L/lib -L/usr/lib -L/usr/local/lib
 UNAME := 	$(shell uname)
 
-ifeq ($($(UNAME),FreeBSD)
-	CC = clang
+ifeq ($(UNAME),FreeBSD)
+	CC = 	clang
 endif
-ifeq ($($(UNAME),OpenBSD)
-	CC = clang
+
+ifeq ($(UNAME),OpenBSD)
+	CC = 	clang
 endif
 
 udev_test:
-	$(CC) $(CFLAGS) -o udev_test udev_test.c -ludev
+	$(CC) $(CFLAGS) $(LDFLAGS) -o udev_test2 udev_test2.c -ludev
+	$(CC) $(CFLAGS) $(LDFLAGS) -o udev_test udev_test.c -ludev
 
 all: udev_test
 
 clean:
-	rm -f udev_test
+	rm -f udev_test udev_test2
--- udev_test.c
+++ udev_test.c
@@ -34,6 +34,7 @@ int main()
 	struct udev_list_entry *devices, *dev_list_entry;
 
 	/* create udev object */
+	printf("D: udev_new()\n");
 	udev = udev_new();
 	if (!udev) {
 		fprintf(stderr, "Cannot create udev context.\n");
@@ -41,22 +42,27 @@ int main()
 	}
 
 	/* create enumerate object */
+	printf("D: udev_enumerate_new()\n");
 	enumerate = udev_enumerate_new(udev);
 	if (!enumerate) {
 		fprintf(stderr, "Cannot create enumerate context.\n");
 		return 1;
 	}
 
-	udev_enumerate_add_match_subsystem(enumerate, "block");
+	printf("D: udev_enumerate_add_match_subsystem()\n");
+	udev_enumerate_add_match_subsystem(enumerate, "fido");
+	printf("D: udev_enumerate_scan_devices()\n");
 	udev_enumerate_scan_devices(enumerate);
 
 	/* fillup device list */
+	printf("D: udev_enumerate_get_list_entry()\n");
 	devices = udev_enumerate_get_list_entry(enumerate);
 	if (!devices) {
 		fprintf(stderr, "Failed to get device list.\n");
 		return 1;
 	}
 
+	printf("D: udev_list_entry_foreach()\n");
 	udev_list_entry_foreach(dev_list_entry, devices) {
 		const char *path, *tmp;
 		unsigned long long disk_size = 0;
@@ -65,28 +71,26 @@ int main()
 		path = udev_list_entry_get_name(dev_list_entry);
 		dev = udev_device_new_from_syspath(udev, path);
 
-		/* skip if device/disk is a partition or loop device */
-		if (strncmp(udev_device_get_devtype(dev), "partition", 9) != 0 &&
-		    strncmp(udev_device_get_sysname(dev), "loop", 4) != 0) {
-			printf("I: DEVNODE=%s\n", udev_device_get_devnode(dev));
-			printf("I: KERNEL=%s\n", udev_device_get_sysname(dev));
-			printf("I: DEVPATH=%s\n", udev_device_get_devpath(dev));
-			printf("I: DEVTYPE=%s\n", udev_device_get_devtype(dev));
+		printf("I: DEVNODE=%s\n", udev_device_get_devnode(dev));
+		printf("I: KERNEL=%s\n", udev_device_get_sysname(dev));
+		printf("I: DEVPATH=%s\n", udev_device_get_devpath(dev));
+		printf("I: DEVTYPE=%s\n", udev_device_get_devtype(dev));
 
-			tmp = udev_device_get_sysattr_value(dev, "size");
-			if (tmp)
-				disk_size = strtoull(tmp, NULL, 10);
+		tmp = udev_device_get_sysattr_value(dev, "size");
+		if (tmp)
+			disk_size = strtoull(tmp, NULL, 10);
 
-			tmp = udev_device_get_sysattr_value(dev, "queue/logical_block_size");
-			if (tmp)
-				block_size = atoi(tmp);
+		tmp = udev_device_get_sysattr_value(dev, "queue/logical_block_size");
+		if (tmp)
+			block_size = atoi(tmp);
 
-			printf("I: DEVSIZE=");
-			if (strncmp(udev_device_get_sysname(dev), "sr", 2) != 0)
-				printf("%lld GB\n", (disk_size * block_size) / 1000000000);
-			else
-				printf("n/a\n");
-		}
+		printf("I: DEVSIZE=");
+		if (strncmp(udev_device_get_sysname(dev), "sr", 2) != 0)
+			printf("%lld GB\n", (disk_size * block_size) / 1000000000);
+		else
+			printf("n/a\n");
+
+		printf("-----");
 
 		/* free dev */
 		udev_device_unref(dev);
